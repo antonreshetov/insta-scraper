585dbad3f68f41c0fcc9c2593ba506fd1fade9d2
Anton Reshetov <reshetov.art@gmail.com>
2018-05-17 23:33:36 +0300

    Refactor: Rewrite in native js on promise.

--- instaScrapper.js
+++ instaScrapper.js
@@ -3,46 +3,39 @@
  * url: https://github.com/antonreshetov
  * @param {string} username - instagram username
  * @param {string} thumbnails - thumbnails (thumbnail_src, display_src)
- * @param {string} element - DOM element
+ * @returns {Promise} - array of images
  */
+function instaScraper(username, thumbnails = 'display_src') {
+  if (!username) throw new Error('Username is required')
 
-function instaScraper(element, username, thumbnails) {
-    if (!username) {
-        throw new Error('Username is required');
-    }
-    if (!element) {
-        throw new Error('DOM element not selected');
-    }
-    var instaURL = 'http://instagram.com/' + username;
-    var instaPageLink = 'http://instagram.com/p/';
-    var thumb = thumbnails || 'display_src';
+  const instaURL = `http://instagram.com/${username}`
+  const instaPageLink = 'http://instagram.com/p/'
+  const thumb = thumbnails
 
-    // YQL query
-    $.getJSON(
-        'http://query.yahooapis.com/v1/public/yql?callback=?',
-        {
-            q: "select * from html where url='" + instaURL + "' and xpath='*'",
-            format: 'json'
-        },
-        function (data) {
-            if (data.query.results != null) {
-                var queryData = data.query.results.html.body.script["0"].content.replace('window._sharedData = ', '').replace(';', '');
-                queryData = $.parseJSON(queryData);
-                var item = queryData.entry_data.ProfilePage["0"].user.media.nodes;
-                for (var i = 0; i < item.length; i++) {
-                    var imgURL = item[i][thumb];
-                    var likes = item[i].likes.count;
-                    var link = instaPageLink + item[i].code;
-                    $(element).append('' +
-                        '<div class="col-xs-4">' +
-                        '<a href="' + link + '">' +
-                        '<img src="' + imgURL + '" alt="" class="img-responsive">' +
-                        '<p class="text-center">Likes: ' + likes + '</p>' +
-                        '</a>' +
-                        '</div>');
-                }
-            } else {
-                throw new Error('Non-existing or private account');
-            }
-        })
+  return new Promise((resolve, reject) => {
+    fetch(
+      `http://allorigins.me/get?url=${encodeURIComponent(instaURL)}&callback=?`
+    )
+      .then(res => res.text())
+      .then(data => {
+        data = JSON.parse(
+          data
+            .split('window._sharedData = ')[1]
+            .split(';</script>')[0]
+            .replace(/\\"/g, '"')
+        )
+
+        if (Object.keys(data.entry_data).length !== 0) {
+          resolve(
+            data.entry_data.ProfilePage[0].graphql.user
+              .edge_owner_to_timeline_media.edges
+          )
+        } else {
+          reject('User name is invalid or account is empty')
+        }
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
 }
